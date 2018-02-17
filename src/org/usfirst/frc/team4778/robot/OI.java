cf57f176133d1dbb251d5d86187f08ef7753da4a
ethanejohnsons <ethan@ejohnsons.com>
2018-02-16 18:06:22 -0600

    Added controls for lifting the cube manipulator

--- src/org/usfirst/frc/team4778/robot/OI.java
+++ src/org/usfirst/frc/team4778/robot/OI.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team4778.robot;
 
 import org.usfirst.frc.team4778.robot.commands.Intake;
+import org.usfirst.frc.team4778.robot.commands.Lift;
 import org.usfirst.frc.team4778.robot.commands.Shoot;
 
 import edu.wpi.first.wpilibj.Joystick;
@@ -18,8 +19,14 @@ public class OI {
 	public static Button shoot = new JoystickButton(gamepad, 8);
 	public static Button intake = new JoystickButton(gamepad, 7);
 	
+	public static Button lift_up = new JoystickButton(gamepad, 4);
+	public static Button lift_down = new JoystickButton(gamepad, 2);
+	
 	public OI() {		
 		shoot.whileHeld(new Shoot(0.7));
 		intake.whileHeld(new Intake(0.7));
+		
+		lift_up.whileActive(new Lift(true));
+		lift_down.whileActive(new Lift(false));
 	}
 }
