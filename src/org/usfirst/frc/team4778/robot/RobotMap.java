b983419f8c87c5401bc94b896b1da7ecb04e729b
Aidan Mlenar <atmlenar@gmail.com>
2018-02-20 22:12:05 -0600

    Change motor controller definitions to reflect competition robot

--- src/org/usfirst/frc/team4778/robot/RobotMap.java
+++ src/org/usfirst/frc/team4778/robot/RobotMap.java
@@ -12,25 +12,25 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 public class RobotMap {
 	
 	// Left drivetrain motor controllers
-	public static WPI_TalonSRX m_frontLeft = new WPI_TalonSRX(3);
-	public static WPI_TalonSRX m_rearLeft  = new WPI_TalonSRX(4);
+	public static WPI_TalonSRX m_frontLeft = new WPI_TalonSRX(7);
+	public static WPI_TalonSRX m_rearLeft  = new WPI_TalonSRX(9);
 	static SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_rearLeft);
 	
 	// Right drivetrain motor controllers
-	public static WPI_TalonSRX m_frontRight = new WPI_TalonSRX(1);
-	public static WPI_TalonSRX m_rearRight  = new WPI_TalonSRX(2);
+	public static WPI_TalonSRX m_frontRight = new WPI_TalonSRX(10);
+	public static WPI_TalonSRX m_rearRight  = new WPI_TalonSRX(6);
 	static SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_rearRight);
 
-	// Cube manipulator motor controllers
-	public static WPI_TalonSRX m_cubeBackLeft   = new WPI_TalonSRX(5);
-	public static WPI_TalonSRX m_cubeFrontLeft  = new WPI_TalonSRX(6);
-	public static WPI_TalonSRX m_cubeBackRight  = new WPI_TalonSRX(7);
-	public static WPI_TalonSRX m_cubeFrontRight = new WPI_TalonSRX(8);
+	// Cube manipulator motor controllers (relative to robot's perspective)
+	public static WPI_TalonSRX m_cubeBackLeft   = new WPI_TalonSRX(8);
+	public static WPI_TalonSRX m_cubeFrontLeft  = new WPI_TalonSRX(2);
+	public static WPI_TalonSRX m_cubeBackRight  = new WPI_TalonSRX(4);
+	public static WPI_TalonSRX m_cubeFrontRight = new WPI_TalonSRX(3);
 	public static SpeedControllerGroup m_cubeMotors = new SpeedControllerGroup(m_cubeBackLeft,m_cubeFrontLeft,m_cubeBackRight,m_cubeFrontRight);
 	
 	// Grabber motor controllers
-	public static WPI_TalonSRX m_grabberLeft  = new WPI_TalonSRX(0);
-	public static WPI_TalonSRX m_grabberRight = new WPI_TalonSRX(9);
+	public static WPI_TalonSRX m_grabberLeft  = new WPI_TalonSRX(1);
+	public static WPI_TalonSRX m_grabberRight = new WPI_TalonSRX(5);
 	public static SpeedControllerGroup m_grabMotors = new SpeedControllerGroup(m_grabberLeft, m_grabberRight);
 	
 	// Lift solenoids
