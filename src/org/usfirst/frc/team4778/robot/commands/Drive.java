0bd31534606012b2b67a7b8cb55a4281f5f289e1
Stormbots <peyton@ejohnsons.com>
2018-02-28 18:18:37 -0600

    Ryan did something

--- src/org/usfirst/frc/team4778/robot/commands/Drive.java
+++ src/org/usfirst/frc/team4778/robot/commands/Drive.java
@@ -19,7 +19,7 @@ public class Drive extends Command {
 
 	@Override
 	protected void execute() {
-		Robot.m_drive.tankDrive(OI.joystickLeft.getY(), -OI.joystickRight.getY(), true);
+		Robot.m_drive.arcadeDrive(-OI.joystickRight.getX(), OI.joystickLeft.getY(), true);
 	}
 
 	@Override
