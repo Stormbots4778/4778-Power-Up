b182b68fc0205e5a5f1ffc3ef6437faef3daa4d1
Aidan Mlenar <atmlenar@gmail.com>
2018-03-22 17:27:54 -0500

    Add intake and shoot position commands

---
+++ src/org/usfirst/frc/team4778/robot/commands/IntakePosition.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team4778.robot.commands;
+
+import org.usfirst.frc.team4778.robot.Robot;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+
+/*
+ * IntakePosition.java
+ * Puts robot into intake position eg: Expands grabbers and puts cube manipulator down
+ */
+public class IntakePosition extends Command {
+
+	private boolean isFinished = false;
+	private double endTime;
+    
+    public IntakePosition() {
+    	requires(Robot.grabber);
+    	requires(Robot.cubemanipulator);
+    }
+
+    protected void initialize() {
+    	endTime = Timer.getFPGATimestamp() + 3; // Sets timer for 3 seconds
+    	Robot.grabber.fold(false);
+    }
+
+    protected void execute() {
+    	if (Timer.getFPGATimestamp() >= endTime) {
+        	Robot.cubemanipulator.lift(false); // Sets lift down
+    		isFinished = true;
+		}
+    }
+
+    protected boolean isFinished() {
+        return isFinished;
+    }
+
+    protected void end() {}
+    
+    protected void interrupted() {
+    	end();
+    }
+}
