078ed00f230f66d2c02c36e6aa28c67f9d64fac2
ethanejohnsons <ethan@ejohnsons.com>
2018-03-22 19:53:46 -0500

    Finished AutoLeft

--- src/org/usfirst/frc/team4778/robot/commands/AutoLeft.java
+++ src/org/usfirst/frc/team4778/robot/commands/AutoLeft.java
@@ -13,36 +13,43 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class AutoLeft extends CommandGroup {
 
-    public AutoLeft(int priority) {
+    public AutoLeft(int priority, boolean shouldWait) {
     		// Init
-    		addSequential(new AutoTimer(3));
+    		if(shouldWait) addSequential(new AutoTimer(3));
     		addSequential(new Lift(true));
 		addSequential(new FoldGrabber(false));
 		
 		// Drive
 		if(Robot.gameData.charAt(priority) == 'L') {
-			if(priority == 0) { // Switch
-				addSequential(new AutoEncoderDrive(0.7, 152));
-				addSequential(new AutoEncoderTurn(0.7, Math.PI/2)); // 90 degrees cw
-				addSequential(new AutoEncoderDrive(0.7, 20));
-				addParallel(new Shoot(0.35));
-				addSequential(new AutoTimer(0.5));
-				addSequential(new Push());
+			if(priority == 0) {
+				switchAuto();
 			} else {
-//				addSequential(new AutoEncoderDrive(0.7, 324));
-//				addSequential(new AutoEncoderTurn(0.7, Math.PI/2)); // 90 degrees cw
-//				addSequential(new AutoEncoderDrive(0.7, -20)); // ???
-//				addParallel(new Shoot(0.8));
-//				addSequential(new AutoTimer(1));
-//				addSequential(new Push());
+				scaleAuto();
 			}
 		} else if(Robot.gameData.charAt(priority == 0 ? 1 : 0) == 'L') {
-			//addSequential(new AutoEncoderDrive(0.7, ?));
-			//addSequential(new TurnToAngle(90));
-			//addParallel(new Shoot(0.8));
-			//addSequential(new AutoTimer(1));
-			//addSequential(new Push());
+			if(priority == 0) {
+				switchAuto();
+			} else {
+				scaleAuto();
+			}
 		}
-		//addSequential(new AutoTimerDrive(0.7, 2));
+    }
+    
+    public void switchAuto() {
+    		addSequential(new AutoEncoderDrive(0.7, 152));
+		addSequential(new AutoEncoderTurn(0.7, Math.PI/2)); // 90 degrees cw
+		addSequential(new AutoEncoderDrive(0.7, 20));
+		addParallel(new Shoot(0.35));
+		addSequential(new AutoTimer(0.5));
+		addSequential(new Push());
+    }
+    
+    public void scaleAuto() {
+    		addSequential(new AutoEncoderDrive(0.7, 276));
+		addSequential(new AutoEncoderTurn(0.7, 1.1)); // ~63 degrees cw
+		addSequential(new AutoEncoderDrive(0.7, -25.5)); // ???
+		addParallel(new Shoot(0.8));
+		addSequential(new AutoTimer(1));
+		addSequential(new Push());
     }
 }
