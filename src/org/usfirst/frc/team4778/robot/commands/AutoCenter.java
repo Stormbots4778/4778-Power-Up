4e8e53ef670b9824614dbf8788943a1ddbe0419b
peytonejohnsons <peyton@ejohnsons.com>
2018-03-26 15:30:22 -0500

    Probably Fixed AutoSide

--- src/org/usfirst/frc/team4778/robot/commands/AutoCenter.java
+++ src/org/usfirst/frc/team4778/robot/commands/AutoCenter.java
@@ -15,37 +15,37 @@ public class AutoCenter extends CommandGroup {
     		// Init
     		if(shouldWait) addSequential(new AutoTimer(3));
     		addSequential(new Lift(true));
-		addSequential(new FoldGrabber(false));
+    		addSequential(new FoldGrabber(false));
 		
-		if(Robot.gameData.charAt(0) == 'L') {
+    		if(Robot.gameData.charAt(0) == 'L') {
 			
-			// Drive
-			addSequential(new AutoEncoderDrive(0.7, 37)); // Drive forward 37 inches (3' 1")
-			addSequential(new AutoEncoderTurn(0.7, -0.53)); // ~30 degrees ccw
-			addSequential(new AutoEncoderDrive(0.7, 69)); // Drive forward 69 inches (5' 9")
-			addSequential(new AutoEncoderTurn(0.7, 0.53)); // ~30 degrees cw
-			addSequential(new AutoEncoderDrive(0.7, 34)); // Drive forward 34 inches (2' 10")
+					// Drive
+					addSequential(new AutoEncoderDrive(0.7, 37)); // Drive forward 37 inches (3' 1")
+					addSequential(new AutoEncoderTurn(0.7, -0.53)); // ~30 degrees ccw
+					addSequential(new AutoEncoderDrive(0.7, 69)); // Drive forward 69 inches (5' 9")
+					addSequential(new AutoEncoderTurn(0.7, 0.53)); // ~30 degrees cw
+					addSequential(new AutoEncoderDrive(0.7, 34)); // Drive forward 34 inches (2' 10")
+				
+		    		// Shoot
+		    		addParallel(new Shoot(0.35)); // 35% power
+		    		addSequential(new AutoTimer(0.5)); // give it half a second to spin up
+		    		addSequential(new Push());
 			
-	    		// Shoot
-	    		addParallel(new Shoot(0.35)); // 35% power
-	    		addSequential(new AutoTimer(0.5)); // give it half a second to spin up
-	    		addSequential(new Push());
+    		} else {
 			
-		} else {
-			
-			// Drive
-			addSequential(new AutoEncoderDrive(0.7, 37)); // Drive forward 37 inches (3' 1")
-			addSequential(new AutoEncoderTurn(0.7, 0.63)); // ~36 degrees cw
-			addSequential(new AutoEncoderDrive(0.7, 60)); // Drive forward 60 inches (5')
-			addSequential(new AutoEncoderTurn(0.7, -0.63)); // ~36 degrees ccw
-			addSequential(new AutoEncoderDrive(0.7, 34)); // Drive forward 34 inches (2' 10")
-			
-	    		// Shoot
-	    		addParallel(new Shoot(0.35)); // 35% power
-	    		addSequential(new AutoTimer(0.5)); // give it half a second to spin up
-	    		addSequential(new Push());
+					// Drive
+					addSequential(new AutoEncoderDrive(0.7, 37)); // Drive forward 37 inches (3' 1")
+					addSequential(new AutoEncoderTurn(0.7, 0.63)); // ~36 degrees cw
+					addSequential(new AutoEncoderDrive(0.7, 60)); // Drive forward 60 inches (5')
+					addSequential(new AutoEncoderTurn(0.7, -0.63)); // ~36 degrees ccw
+					addSequential(new AutoEncoderDrive(0.7, 34)); // Drive forward 34 inches (2' 10")
+				
+		    		// Shoot
+		    		addParallel(new Shoot(0.35)); // 35% power
+		    		addSequential(new AutoTimer(0.5)); // give it half a second to spin up
+		    		addSequential(new Push());
 	    		
-		}
-
+    		}
+    		
     }
 }
--- src/org/usfirst/frc/team4778/robot/commands/AutoSide.java
+++ src/org/usfirst/frc/team4778/robot/commands/AutoSide.java
@@ -18,50 +18,64 @@ public class AutoSide extends CommandGroup {
     		// Init
     		if(shouldWait) addSequential(new AutoTimer(3));
     		addSequential(new Lift(true));
-		addSequential(new FoldGrabber(false));
+    		addSequential(new FoldGrabber(false));
 		
-		// Everything else
-		if(priority == 0) {
-			switchAuto(side);
-		} else {
-			scaleAuto(side);
-		}
-
+    		// Logic Stuff
+    		if(priority == 0) {
+    				if(Robot.gameData.charAt(0) == side) {
+    						switchAuto(side);
+    				} else if(Robot.gameData.charAt(1) == side) {
+    						scaleAuto(side);
+    				} else {
+    						crossLineAuto(side);
+    				}
+    		
+    		} else if(priority == 1) {
+    				if(Robot.gameData.charAt(1) == side) {
+    						scaleAuto(side);
+    				} else if(Robot.gameData.charAt(0) == side) {
+    						switchAuto(side);
+    				} else {
+    						crossLineAuto(side);
+    				}
+    		
+    		}
     }
     
     private void switchAuto(char side) {
-    		if(Robot.gameData.charAt(0) == side) {
-        		addSequential(new AutoEncoderDrive(0.7, 152)); // Drive forward 152 inches (12' 8")
-        		
-        		if(side == 'L') addSequential(new AutoEncoderTurn(0.7, Math.PI/2)); // 90 degrees cw for left
-        		else if(side == 'R') addSequential(new AutoEncoderTurn(0.7, -Math.PI/2)); // or 90 degrees ccw for right
-        		
-        		addSequential(new AutoEncoderDrive(0.7, 20)); //Drive forward against the switch (20 inches)
-        		
-        		// Shoot
-        		addParallel(new Shoot(0.35)); // 35% power
-        		addSequential(new AutoTimer(0.5)); // give it half a second to spin up
-        		addSequential(new Push());
-    		} else {
-    			scaleAuto(side);
-    		}
+			addSequential(new AutoEncoderDrive(0.7, 152)); // Drive forward 152 inches (12' 8")
+			
+			if(side == 'L') addSequential(new AutoEncoderTurn(0.7, Math.PI/2)); // 90 degrees cw for left
+			else if(side == 'R') addSequential(new AutoEncoderTurn(0.7, -Math.PI/2)); // or 90 degrees ccw for right
+			
+			addSequential(new AutoEncoderDrive(0.7, 20)); //Drive forward against the switch (20 inches)
+			
+			// Shoot
+			addParallel(new Shoot(0.35)); // 35% power
+			addSequential(new AutoTimer(0.5)); // give it half a second to spin up
+			addSequential(new Push());
     }
     
     private void scaleAuto(char side) {
-    		if(Robot.gameData.charAt(1) == side) {
-	    		addSequential(new AutoEncoderDrive(0.7, 276)); // Dirve forward 276 inches (23')
-	    		
-	    		if(side == 'L') addSequential(new AutoEncoderTurn(0.7, 1.1)); // ~63 degrees cw for left
-	    		else if(side == 'R')  addSequential(new AutoEncoderTurn(0.7, -1.1)); // ~63 degrees ccw for right
-	    			
-	    		addSequential(new AutoEncoderDrive(0.7, -25.5)); // Drive backwards 25.5 inches (2' 1.5")
+			addSequential(new AutoEncoderDrive(0.7, 276)); // Drive forward 152 inches (23')
 			
-	    		// Shoot
+			if(side == 'L') addSequential(new AutoEncoderTurn(0.7, 1.1)); // ~63 degrees cw for left
+			else if(side == 'R')  addSequential(new AutoEncoderTurn(0.7, -1.1)); // ~63 degrees ccw for right
+			
+			addSequential(new AutoEncoderDrive(0.7, -25.5)); // Drive backwards 25.5 inches (2' 1.5")
+			
+			// Shoot
 			addParallel(new Shoot(0.8)); // 80% power
 			addSequential(new AutoTimer(1)); // give it one second to spin up
 			addSequential(new Push());
-    		} else {
-    			switchAuto(side);
-    		}
+    }
+    
+    private void crossLineAuto(char side) {
+    	addSequential(new AutoEncoderDrive(0.7, 214)); // Drive forward 152 inches (17' 10") ESTIMATE, will measure later
+    	
+		if(side == 'L') addSequential(new AutoEncoderTurn(0.7, Math.PI/2)); // 90 degrees cw for left
+		else if(side == 'R') addSequential(new AutoEncoderTurn(0.7, -Math.PI/2)); // or 90 degrees ccw for right
+		
+		addSequential(new AutoEncoderDrive(0.7, 144)); // Drive forward 152 inches (12') ESTIMATE, will measure later
     }
 }
