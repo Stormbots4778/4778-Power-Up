fa8a932387ab2dfc70e3cdfbdd74fa3d271f773d
ethanejohnsons <ethan@ejohnsons.com>
2018-03-26 12:33:25 -0500

    Fixed AutoCenter so it doesn't have a mysterious method near the bottom

--- src/org/usfirst/frc/team4778/robot/commands/AutoCenter.java
+++ src/org/usfirst/frc/team4778/robot/commands/AutoCenter.java
@@ -48,20 +48,4 @@ public class AutoCenter extends CommandGroup {
 		}
 
     }
-    
-    private void switchAuto(char side) {
-    		if(Robot.gameData.charAt(0) == side) {
-        		addSequential(new AutoEncoderDrive(0.7, 152)); // Drive forward 152 inches (12' 8")
-        		
-        		if(side == 'L') addSequential(new AutoEncoderTurn(0.7, Math.PI/2)); // 90 degrees cw for left
-        		else if(side == 'R') addSequential(new AutoEncoderTurn(0.7, -Math.PI/2)); // or 90 degrees ccw for right
-        		
-        		addSequential(new AutoEncoderDrive(0.7, 20)); //Drive forward against the switch (20 inches)
-        		
-        		// Shoot
-        		addParallel(new Shoot(0.35)); // 35% power
-        		addSequential(new AutoTimer(0.5)); // give it half a second to spin up
-        		addSequential(new Push());
-    		}
-    }
 }
