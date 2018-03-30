ad1112c5739bea83229f907aca312428a183e748
ethanejohnsons <ethan@ejohnsons.com>
2018-03-30 09:53:18 -0500

    Changed final distance in AutoCenter

--- src/org/usfirst/frc/team4778/robot/commands/AutoCenter.java
+++ src/org/usfirst/frc/team4778/robot/commands/AutoCenter.java
@@ -21,9 +21,9 @@ public class AutoCenter extends CommandGroup {
 					// Drive
 					addSequential(new AutoEncoderDrive(0.7, 37, 2)); // Drive forward 37 inches (3' 1")
 					addSequential(new AutoEncoderTurn(0.7, -1.04, 1.5)); // ~77 degrees ccw
-					addSequential(new AutoEncoderDrive(0.7, 69, 4)); // Drive forward 95 inches (7' 11")
+					addSequential(new AutoEncoderDrive(0.7, 69, 2)); // Drive forward 95 inches (7' 11")
 					addSequential(new AutoEncoderTurn(0.7, 1.04, 1.5)); // ~77 degrees cw
-					addSequential(new AutoEncoderDrive(0.7, 34, 2)); // Drive forward 37 inches (3' 1")
+					addSequential(new AutoEncoderDrive(0.7, 60, 2)); // Drive forward 60 inches (5')
 			
     		} else {
 					//Drive
@@ -31,7 +31,7 @@ public class AutoCenter extends CommandGroup {
 					addSequential(new AutoEncoderTurn(0.7, 0.94, 1.5)); // ~57 degrees cw
 					addSequential(new AutoEncoderDrive(0.7, 60, 2)); // Drive forward 50 inches (4' 2")
 					addSequential(new AutoEncoderTurn(0.7, -0.94, 1.5)); // ~57 degrees ccw
-					addSequential(new AutoEncoderDrive(0.7, 34, 2)); // Drive forward 60 inches (5')
+					addSequential(new AutoEncoderDrive(0.7, 60, 2)); // Drive forward 60 inches (5')
 	    		
     		}
     		// Shoot
